8651da314fd0f2d65741ff2106d01f70fe091114
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-03-08 20:51:25 +0530

    refactored Vector.hpp

--- src/RTWeekend/Vector.hpp
+++ src/RTWeekend/Vector.hpp
@@ -39,6 +39,12 @@ public:
   double length_squared() const {
     return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
   }
+  bool near_zero() const {
+    // Return true if the vector is close to zero in all dimensions.
+    auto s = 1e-8;
+    return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) &&
+           (std::fabs(e[2]) < s);
+  }
 };
 
 using point3 = vec3;
