4525f94280581a671182413438923f4e2fc60810
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-03-12 22:43:52 +0530

    Added global veriables

--- src/RTWeekend/main.cpp
+++ src/RTWeekend/main.cpp
@@ -15,7 +15,7 @@ double hit_sphere(const point3 &center, double radius, const ray &r) {
 }
 
 color ray_color(const ray &r) {
-  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  auto t = hit_sphere(point3(0, 0, -1), 0.66, r);
   if (t > 0.0) {
     vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
     return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
