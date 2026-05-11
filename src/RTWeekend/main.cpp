144aa7c310990239727fb52249ce671c33f84f67
Gaurav Nikam <gauravpn.a2@gmail.com>
2026-05-11 21:50:13 +0530

    cleanup

--- src/RTWeekend/main.cpp
+++ src/RTWeekend/main.cpp
@@ -31,7 +31,7 @@ int main() {
   auto aspect_ratio = 16.0 / 9.0;
   int image_width = 256;
 
-  // Calculate the image height, and ensure that it's at least 1
+  // Calculate the image height, and ensure that it's at least 1.
   int image_height = int(image_width / aspect_ratio);
   image_height = (image_height < 1) ? 1 : image_height;
 
